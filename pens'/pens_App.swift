fcb21afb41780e895a0d7c2c3aa788a5412373ae
sysliar <sysliar@gmail.com>
2023-03-13 16:32:43 +0900

    Initial Commit

---
+++ pens'/pens_App.swift
@@ -0,0 +1,17 @@
+//
+//  pens_App.swift
+//  pens'
+//
+//  Created by Lee Jeong Woo on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct pens_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
