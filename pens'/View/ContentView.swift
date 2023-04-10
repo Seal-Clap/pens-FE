ac14925755b1cb072e1914420256e59912605f7a
박첨벙 <114977536+plopPark@users.noreply.github.com>
2023-04-10 12:33:26 +0900

    design : Home화면 ui 초안

--- pens'/View/ContentView.swift
+++ pens'/View/ContentView.swift
@@ -8,46 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+   
     var body: some View {
-        NavigationView{
-            VStack (spacing: 0){
-                //StartImage
-                Image(systemName: "pencil.line")
-                    .font(.system(size:140))
-                Text("Pens'")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
-                //login
-                NavigationLink(destination:LoginView(), label:{
-                    HStack{
-                        Spacer()
-                        Text("로그인")
-                        Spacer()
-                    }
-                    .padding()
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .frame(width: 250)
-                }).padding()
-                //singup
-                NavigationLink(destination:ResisterView(), label:{
-                    HStack{
-                        Spacer()
-                        Text("회원가입")
-                        Spacer()
-                    }
-                    .padding()
-                    .background(Color.gray)
-                    .foregroundColor(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .frame(width: 250)
-                })
+            NavigationView{
+                VStack (spacing: 0){
+                    //StartImage
+                    Image(systemName: "pencil.line")
+                        .font(.system(size:140))
+                    Text("Pens'")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                    //login
+                    NavigationLink(destination:LoginView(), label:{
+                        HStack{
+                            Spacer()
+                            Text("로그인")
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .frame(width: 250)
+                    }).padding()
+                    //singup
+                    NavigationLink(destination:ResisterView(), label:{
+                        HStack{
+                            Spacer()
+                            Text("회원가입")
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundColor(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .frame(width: 250)
+                    })
+                }
             }
-        }
-        
-            
     }
 }
 
---
+++ pens'/View/HomeView.swift
@@ -0,0 +1,58 @@
+//
+//  HomeView.swift
+//  pens'
+//
+//  Created by 박상준 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var rightOffset: CGFloat = 0
+    var body: some View {
+        NavigationView{
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                    // 좌측 내용
+                    VStack{
+                        Image(systemName: "person.circle") .font(.system(size:50))
+                            .frame(maxWidth: .infinity)
+                            .padding(.top, 60)
+                            .padding(.leading, 100)
+                        // 가로로 지나는 선
+                        Divider()
+                        .background(Color.gray)
+                                .frame(height: 1)
+                                .padding(.bottom, 20)
+                    Spacer()
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    Divider().background(Color.gray)
+                    // 오른쪽 내용
+                    VStack {
+                        HStack{
+                            Text("그룹이름").frame(maxWidth: .infinity)
+                                .padding(.top)
+                                .padding(.leading, -10)
+                                .font(.system(size: 20))
+                            Image(systemName: "plus")
+                                .padding(.top)
+                                .padding(.leading, -50)
+                        }
+                            
+                        Divider()
+                                
+                        Spacer()
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
