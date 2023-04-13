5990f040a4dcf61eb11028c6293f6f7af9e131c3
박첨벙 <114977536+plopPark@users.noreply.github.com>
2023-04-13 14:52:33 +0900

    desigin : Home ui 추가

--- pens'/View/HomeView.swift
+++ pens'/View/HomeView.swift
@@ -9,23 +9,41 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        HStack {
             NavigationSplitView {
-                Text("SideBar")
-                    .navigationSplitViewColumnWidth(400)
+                VStack{
+                    HStack{
+                        Text("그룹 이름")
+                            .font(.title)
+                            .navigationSplitViewColumnWidth(400)
+                            .padding(.leading, 100)
+                        Spacer()
+                        Button(action: {}, label: {Text("초대")}).padding(.leading, -60)
+                    }
+                    
+                    Divider()
+                    Spacer()
+                }
             }
             content: {
-                
+                HStack{
+                    Text("그룹 추가").font(.title2)
+                    Image(systemName: "plus")
+                        .font(.system(size: 25))
+                }
+                Divider()
                 List {
-                    Text("Group")
-                    Text("temp")
+                    HStack{
+                        Image(systemName: "person.circle")
+                            .font(.system(size: 40))
+                            .padding(.leading, -5)
+                        Text("사용자").font(.title3)
+                    }
                 }
                 .navigationSplitViewColumnWidth(150)
             }
             detail: {
                 Text("Detail")
             }
-        }
     }
 }
 
