9ec3f2598272da5385be50873902dda8e85a6a12
sinjs96 <93148114+sinjs96@users.noreply.github.com>
2023-05-02 05:08:13 +0900

    feat : 그룹 추가, 그룹 나가기

--- pens'/API/APIContants.swift
+++ pens'/API/APIContants.swift
@@ -9,9 +9,25 @@ import Foundation
 
 struct APIContants{
     //고정ip
-    static let baseURL = "http://13.125.106.204:8080";
-    //login
-    static let loginURL = baseURL + "/login";
-    //register
-    static let registerURL = baseURL + "/register";
+    static let baseURL = "http://13.125.106.204:8080"
+    //login 로그인
+    static let loginURL = baseURL + "/login"
+    //register 회원가입
+    static let registerURL = baseURL + "/register"
+    //group create 그룹 생성
+    static let groupCreateURL = baseURL + "/group/create";
+    //group delete 그룹 삭제
+    static let groupDeleteURL = baseURL + "/group/delete";
+    //user groups 유저 그룹 목록
+    static let userGroupsURL = baseURL + "/user/groups";
+    //group add-user 그룹 유저 추가
+    static let groupAddUserURL = baseURL + "/group/add-user";
+    //group users 그룹 유저 목록
+    static let groupUsersURL = baseURL + "/group/users";
+    //group delete-user 그룹 유저 삭제 (나가기)
+    static let groupDeleteUserURL = baseURL + "/group/delete-user";
+    //group isAdmin 그룹 방장 확인
+    static let groupIsAdminURL = baseURL + "/group/isAdmin";
+    //group invite 그룹 초대
+    static let groupInviteURL = baseURL + "/group/invite";
 }
---
+++ pens'/Method/GroupDeleteUser.swift
@@ -0,0 +1,45 @@
+//
+//  GroupDeleteUser.swift
+//  pens'
+//
+//  Created by 신지선 on 2023/05/02.
+//
+
+import Foundation
+
+enum GroupAPIError: Error {
+    case serverError
+    case unknownError
+}
+
+class LeaveGroup: ObservableObject {
+    func leaveGroup(groupId: Int, userId: Int, completion: @escaping (Result<Void, GroupAPIError>) -> Void) {
+        guard let url = URL(string: APIContants.groupDeleteUserURL) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(["groupId": groupId, "userId": userId])
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, error == nil else {
+                completion(.failure(.serverError))
+                return
+            }
+
+            if let data = data, let apiResponse = try? JSONDecoder().decode(APIResponse.self, from: data) {
+                if httpResponse.statusCode == 200 && apiResponse.success {
+                    completion(.success(()))
+                } else {
+                    completion(.failure(.unknownError))
+                }
+            } else {
+                completion(.failure(.unknownError))
+            }
+        }.resume()
+    }
+
+    struct APIResponse: Codable {
+        let success: Bool
+        let message: String
+    }
+}
