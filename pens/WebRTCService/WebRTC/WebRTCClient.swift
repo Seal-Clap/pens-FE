8c3645610f0b1e9f4153622ee72bc7ea95aca83f
sysliar <sysliar@gmail.com>
2023-05-19 19:22:40 +0900

    feat: description transmit sequence change #2

--- pens/WebRTCService/WebRTC/WebRTCClient.swift
+++ pens/WebRTCService/WebRTC/WebRTCClient.swift
@@ -64,8 +64,8 @@ class WebRTCClient: NSObject {
                     print("Failed to create offer: \(error?.localizedDescription ?? "")")
                     return
                 }
-            let sdpDescription = self.extractDesc(desc: sdp)
-            
+                let sdpDescription = self.extractDesc(desc: sdp)
+
                 self.peerConnection?.setLocalDescription(sdpDescription, completionHandler: { (error) in
                     if let error = error {
                         debugPrint(error)
@@ -87,7 +87,7 @@ class WebRTCClient: NSObject {
                 return
             }
         })
-        
+
 //        self.peerConnection?.setLocalDescription(sdpDescription, completionHandler: { (error) in
 //            if let error = error {
 //                debugPrint(error)
@@ -107,11 +107,11 @@ class WebRTCClient: NSObject {
                 }
                 let sdpDescription = self.extractDesc(desc: sdp)
 //                self.setLocalSDP(sdpDescription)
-            self.peerConnection?.setLocalDescription(sdpDescription, completionHandler: { (error) in
-                if let error = error {
-                    debugPrint(error)
-                }
-            })
+                self.peerConnection?.setLocalDescription(sdpDescription, completionHandler: { (error) in
+                    if let error = error {
+                        debugPrint(error)
+                    }
+                })
                 guard let jsonData = sdpDescription.jsonData() else { return }
                 self.delegate?.webRTCClient(self, sendData: jsonData, type: "answer")
 
@@ -230,25 +230,25 @@ extension WebRTCClient {
         hasReceivedSdp = true
 //        var extractDesc =
         let sdp = extractDesc(desc: desc)
-        //TODO
-        peerConnection.setRemoteDescription(sdp, completionHandler: { [weak self](error) in
-            if let error = error {
-                dLog(error)
-            }
+//        //TODO
+//        peerConnection.setRemoteDescription(sdp, completionHandler: { [weak self](error) in
+//            if let error = error {
+//                dLog(error)
+//            }
 //            dLog(desc)
-            if desc.type == .offer,
-                self?.peerConnection?.localDescription == nil {
-//                self?.createAnswer()
-//                guard let sdp = self?.extractDesc(desc: desc) else { return }
-                self?.receivedOffer(sdp)
-            }
-            if desc.type == .answer {
-                //  self?.peerConnection?.localDescription == nil {
-                //self?.createAnswer()
-                // guard let sdp = self?.extractDesc(desc: desc) else { return }
-                self?.receivedAnswer(sdp)
-            }
-        })
+        if desc.type == .offer {
+
+            //                self?.createAnswer()
+            //                guard let sdp = self?.extractDesc(desc: desc) else { return }
+            self.receivedOffer(sdp)
+        }
+        if desc.type == .answer {
+            //  self?.peerConnection?.localDescription == nil {
+            //self?.createAnswer()
+            // guard let sdp = self?.extractDesc(desc: desc) else { return }
+            self.receivedAnswer(sdp)
+        }
+//        })
     }
 
     func drainMessageQueue() {
