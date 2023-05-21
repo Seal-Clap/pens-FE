25064a7c447a12bd2d79fe9e2bcccbace4e89f38
sysliar <sysliar@gmail.com>
2023-05-21 16:23:12 +0900

    feat: channel change socket disconnect

--- pens/View/VoiceChannelView.swift
+++ pens/View/VoiceChannelView.swift
@@ -38,6 +38,7 @@ struct VoiceChannelView: View {
                     }
                         .padding(.leading)
                         .onTapGesture {
+                            self.viewModel.disconnect()
                             self.viewModel.connectRoom(roomID: String(channel.channelId))
                     }
                 }
