d2d34c9f884407af2bbdda2b6627770d4d0ead06
박첨벙 <114977536+plopPark@users.noreply.github.com>
2023-05-30 17:39:55 +0900

    fix : 파일 업로드, pdf, draw 모두 가능

--- pens/Method/FileUpload.swift
+++ pens/Method/FileUpload.swift
@@ -9,8 +9,22 @@ import Foundation
 import Alamofire
 
 func uploadFile(groupId: Int, fileUrl: URL, completion: @escaping () -> Void) {
-    guard let url = URL(string: "\(APIContants.fileUploadURL)?groupId=\(groupId)")
-        else { return }
+    let url = URL(string: "\(APIContants.fileUploadURL)?groupId=\(groupId)")!
+    
+    var didStartAccessing = false
+    
+    // If the file is a .pdf, try to start accessing the security scoped resource
+    if fileUrl.pathExtension.lowercased() == "pdf" {
+        didStartAccessing = fileUrl.startAccessingSecurityScopedResource()
+        
+        guard didStartAccessing else {
+            // Handle the failure here.
+            return
+        }
+    }
+    
+    // Make sure you release the security-scoped resource when you finish, only if we started accessing it
+    defer { if didStartAccessing { fileUrl.stopAccessingSecurityScopedResource() } }
     
     do {
         let fileData = try Data(contentsOf: fileUrl)
@@ -19,10 +33,9 @@ func uploadFile(groupId: Int, fileUrl: URL, completion: @escaping () -> Void) {
         }, to: url, method: .post).validate(statusCode: 200..<300)
             .response { response in
                 debugPrint(response)
-                completion()
+                completion() // call completion handler here
             }
     } catch {
         print("Unable to load data: \(error)")
     }
 }
-
